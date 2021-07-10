79f00ab1e2351fbf7d107a0ae7f19c90e500848f
crybto <69460458+crybto@users.noreply.github.com>
2021-07-10 16:45:10 -0700

    Add files via upload

---
+++ task5.php
@@ -0,0 +1,22 @@
+<!-- $array = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+
+Create a PHP script which displays the capital and country name from the above array $array. Sort the list by the capital of the country.
+
+=================================
+Write a PHP script to split the following string.
+Sample string : '672319'
+Output : 67:23:19 -->
+
+<?php
+// part 1
+$array = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+asort($array);
+foreach($array as $key => $value){
+    echo $value.' capital of '.$key.'<br>';
+}
+// ================================
+//part 2
+$sample_string = '672319';
+$output = $sample_string[0].$sample_string[1].":".$sample_string[2].$sample_string[3].":".$sample_string[4].$sample_string[5];
+echo $output;
+?>
